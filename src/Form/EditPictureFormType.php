66eec8ff51c6c06756ad17db1d33f741a4fb6c85
Aurélie <aur.gilet@gmail.com>
2021-10-06 21:47:18 +0200

    correct some errors in other cat care pages template and form

--- src/Form/EditPictureFormType.php
+++ src/Form/EditPictureFormType.php
@@ -14,7 +14,8 @@ class EditPictureFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('picture', FileType::class, array('data_class' => null), [
+            ->add('picture', FileType::class, [
+                'data_class' => null,
                 'label' => 'Photo de Profil',                
             ])
             ->add('submit', SubmitType::class, [
