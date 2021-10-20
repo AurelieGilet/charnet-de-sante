c950a34e59e873280ea70421da579d8ce5103452
Aurélie <aur.gilet@gmail.com>
2021-10-20 19:28:16 +0200

    FAQ Form Type

--- src/Form/AdminFAQFormType.php
+++ src/Form/AdminFAQFormType.php
@@ -6,10 +6,11 @@ use App\Entity\FAQ;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
-class FAQFormType extends AbstractType
+class AdminFAQFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
