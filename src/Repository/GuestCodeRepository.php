b97f662decfcdde95d970583d5f6f11ef7e09ca7
Aurélie <aur.gilet@gmail.com>
2021-11-22 16:08:54 +0100

    guest & guestCode repo

--- src/Repository/GuestCodeRepository.php
+++ src/Repository/GuestCodeRepository.php
@@ -18,33 +18,4 @@ class GuestCodeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, GuestCode::class);
     }
-
-    // /**
-    //  * @return GuestCode[] Returns an array of GuestCode objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('g.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?GuestCode
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
--- src/Repository/GuestRepository.php
+++ src/Repository/GuestRepository.php
@@ -35,33 +35,4 @@ class GuestRepository extends ServiceEntityRepository implements PasswordUpgrade
         $this->_em->persist($user);
         $this->_em->flush();
     }
-
-    // /**
-    //  * @return Guest[] Returns an array of Guest objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('g.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Guest
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
